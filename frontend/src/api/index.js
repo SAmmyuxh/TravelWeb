92a4e882c3c9dfda0e5f3ee2eaabe0b0968ad628
SAmmyuxh <samruddhshubhadarshi@gmail.com>
2025-06-14 11:42:13 +0530

    checking

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 const API = axios.create({
   baseURL: `${import.meta.env.VITE_REACT_APP_BACKEND_URL}/api`,
 });
-
+console.log(import.meta.env.VITE_REACT_APP_BACKEND_URL);
 export const fetchDestinations = () => API.get('/destinations');
 export const fetchTopPackages = () => API.get('/packages/top-selling');
